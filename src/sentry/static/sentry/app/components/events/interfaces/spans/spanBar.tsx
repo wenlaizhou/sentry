f8092ceb58cd7be6f8ef9c6e173183f340a4a2a1
Robin Rendle <robinjrendle@gmail.com>
2020-08-04 13:34:04 -0700

    replaced old gray200 -> new gray100

--- src/sentry/static/sentry/app/components/events/interfaces/spans/spanBar.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/spanBar.tsx
@@ -877,7 +877,7 @@ const SpanRowCellContainer = styled('div')<SpanRowCellProps>`
   user-select: none;
 
   &:hover > div[data-type='span-row-cell'] {
-    background-color: ${p => (p.showDetail ? p.theme.gray800 : p.theme.gray200)};
+    background-color: ${p => (p.showDetail ? p.theme.gray800 : p.theme.gray100)};
   }
 `;
 
